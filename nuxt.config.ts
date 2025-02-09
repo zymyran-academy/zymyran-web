1bc69c5d11a82f1bdd59bf741c5099d2506be0d6
thatoranzhevyy <thatoranzhevyy@gmail.com>
2025-02-09 20:39:10 +0500

    реформат кода

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: {enabled: true},
   modules: ['@nuxt/ui'],
   // что бы отключить черную тему
   colorMode: {preference: "light"},
